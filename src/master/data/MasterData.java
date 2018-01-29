b094435a1bfbf34cf64ee31f497c5942f5a6700c
Utkarsh <utkarsh@gmail.com>
2018-01-29 09:21:59 +0530

    First commit - Tabs added

---
+++ src/master/data/MasterData.java
@@ -0,0 +1,13 @@
+package master.data;
+
+
+public class MasterData {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
